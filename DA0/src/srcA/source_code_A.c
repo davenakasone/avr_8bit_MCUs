4a2c822af1f2a7392dd063eb88eefd9bc3c36715
davenakasone <davenakasone@gmail.com>
2022-01-17 17:43:07 -0800

    more

---
+++ DA0/src/srcA/source_code_A.c
@@ -0,0 +1,22 @@
+/*
+    source code for task A
+*/
+
+#include <Arduino.h>
+
+
+void setup ()
+{
+    // set something up
+}
+
+
+////~~~~
+
+
+void loop ()
+{
+    // do something
+}
+
+////////~~~~~~~~END>  source_code_A.c
---
+++ DA0/src/srcB/source_code_B.c
@@ -0,0 +1,22 @@
+/*
+    source code for task B
+*/
+
+#include <Arduino.h>
+
+
+void setup ()
+{
+    // set something up
+}
+
+
+////~~~~
+
+
+void loop ()
+{
+    // do something
+}
+
+////////~~~~~~~~END>  source_code_B.c
